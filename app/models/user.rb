2b299352fcdb2206e06891532338b4922dc32342
Roodz Fernando Fleurant <roodzfernando@Roodzs-Air.router>
2020-02-10 22:49:00 -0500

    Add the authenticated? method

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,8 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end

e690ad912c1d38267387044bd8554bec4b39f7be
RoodzFernando <fleurantrfernando@gmail.com>
2020-02-11 15:00:16 -0500

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,12 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   end
 
   def authenticated?(remember_token)
+      return false if remember_digest.nil?
+
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+   def forget
+    update_attribute(:remember_digest, nil)
   end
 end

49cac64c4b4031d58cbf0e786855a48ec2730d96
Roodz Fernando Fleurant <roodzfernando@Roodzs-Air.router>
2020-02-24 16:36:53 -0500

    Write test for activated users

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,6 +37,17 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     update_attribute(:remember_digest, nil)
   end
 
+    # Activates an account.
+    def activate
+      update_attribute(:activated,    true)
+      update_attribute(:activated_at, Time.zone.now)
+    end
+
+    # Sends activation email.
+    def send_activation_email
+      UserMailer.account_activation(self).deliver_now
+    end
+
   private
     def downcase_email
       self.email = email.downcase
